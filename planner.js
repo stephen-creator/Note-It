8d56f265f916fc1a32af65aebd0f3653f1878fbc
stephen-creator <stephenlin096@gmail.com>
2024-02-27 23:06:40 -0500

    second commit

--- planner.js
+++ planner.js
@@ -7,42 +7,42 @@ menu.addEventListener('click', function() {
 });
 
 
-//TO-DO LIST
-// const inputBox = document.getElementById("input-box");
-// const listContainer = document.getElementById("list-container");
+// TO-DO LIST
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-// function addTask(){
-//     if(inputBox.value === ''){
-//         alert("You must write something!");
-//     }
-//     else{
-//         let li = document.createElement("li");
-//         li.innerHTML = inputBox.value;
-//         listContainer.appendChild(li);
-//         let span = document.createElement("span");
-//         span.innerHTML = "\u00d7";
-//         li.appendChild(span);
-//     }
-//     inputBox.value = "";
-//     saveData();
-// }
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
 
-// listContainer.addEventListener("click", function(e){
-//     if(e.target.tagName === "LI"){
-//         e.target.classList.toggle("checked");
-//         saveData();
-//     }
-//     else if(e.target.tagName === "SPAN"){
-//         e.target.parentElement.remove();
-//         saveData();
-//     }
-// }, false);
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
 
-// //saving data on local browser so everytime you refresh screen, it's still there
-// function saveData(){
-//     localStorage.setItem("data", listContainer.innerHTML);
-// }
-// function showTask(){
-//     listContainer.innerHTML = localStorage.getItem("data");
-// }
-// showTask();
+//saving data on local browser so everytime you refresh screen, it's still there
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
